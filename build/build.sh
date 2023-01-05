314ebe434916480de868c76150b920768553b968
hwong23 <hwong23@gmail.com>
2023-01-05 16:53:43 -0500

    build 2

--- build/build.sh
+++ build/build.sh
@@ -88,10 +88,10 @@ fi
 if [ "${BUILD_DOCX}" = "true" ]; then
   echo >&2 "Exporting Word Docx manuscript"
   for f in content/*.md; do
-    pandoc --verbose \
-      --data-dir="$PANDOC_DATA_DIR" \
-      --defaults=common.yaml \
-      --defaults=docx.yaml
+#   pandoc --verbose \
+#     --data-dir="$PANDOC_DATA_DIR" \
+#     --defaults=common.yaml \
+#     --defaults=docx.yaml
 
     echo pandoc --data-dir=$"PANDOC_DATA_DIR" --output-file=output/"${f%.md}.docx";
   done
