bb370aa90b4874b0abbf213b1f5e4d5daa443724
hwong23 <hwong23@gmail.com>
2023-01-06 12:18:09 -0500

    ci 6

--- build/build.sh
+++ build/build.sh
@@ -35,7 +35,7 @@ if [ "${BUILD_DOCX}" = "true" ]; then
   for f in content/*.md; do
     basenameFILE=${f##*/};
     
-    echo --data-dir="$PANDOC_DATA_DIR" --output=output/"${basenameFILE%.md}.docx" $f id="${github.run_id}" commit="${MANUBOT.CI_SOURCE.COMMIT}";
+    echo --data-dir="$PANDOC_DATA_DIR" --output=output/"${basenameFILE%.md}.docx" $f id=${{ github.event.inputs.BUILD_PDF }};
   done
 
   pandoc --verbose \
